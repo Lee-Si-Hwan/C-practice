3e11a70728b05f105539a46a3330a343b0728efd
Lee-Si-Hwan <58382954+Lee-Si-Hwan@users.noreply.github.com>
2019-12-19 23:48:30 +0900

    Add files via upload

---
+++ C-예제2-2(주석).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*메인 함수 실행*/
+int main(void)
+{
+	printf("Hello world\n");
+	/*Hello world를 출력함*/
+	
+	/*
+	이렇게 
+	여러 줄도 
+	주석 가능
+	*/
+
+	//이러면 //뒤에 있는 모든 문장은 주석처리가 됨
+	return 0;
+}
