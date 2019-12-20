a5977dfbc53ce10463d76f6cf5a23d1f8accedfe
Lee-Si-Hwan <58382954+Lee-Si-Hwan@users.noreply.github.com>
2019-12-21 00:12:18 +0900

    Add files via upload

---
+++ C-예제3-2(연산자).cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 9;
+	int num2 = 2;
+
+	printf("%d + %d = %d\n", num1, num2, num1 + num2);
+	printf("%d - %d = %d\n", num1, num2, num1 - num2);
+	printf("%d x %d = %d\n", num1, num2, num1 * num2);
+	printf("%d / %d 의 몫 = %d\n", num1, num2, num1 / num2);
+	printf("%d / %d 의 나머지 = %d\n", num1, num2, num1 % num2);
+
+	return 0;
+
+}
