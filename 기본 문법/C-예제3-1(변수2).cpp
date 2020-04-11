cc0aa617dfdad902e10fd1ae6af2b48bfe786c85
Lee-Si-Hwan <lhs0831lkh@naver.com>
2020-04-11 19:28:49 +0900

    updata

---
+++ 기본 문법/C-예제3-1(변수2).cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 3;
+	int num2 = 4;
+	int result = num1 + num2;// 변수선언은 앞에 먼저 해야 에러가 안 뜬다
+
+	printf("덧셈 결과: %d\n", result);
+	printf("%d + %d = %d\n", num1, num2, result);
+	printf("%d와(과) %d의 합은 %d입니다\n", num1, num2, result);
+
+	return 0;
+}
---
+++ 기본 문법/C-예제3-2(증가, 감소 연산자).cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 10, num2 = 10;
+
+	printf("num1 = %d\n", num1);
+	printf("num1++ = %d\n", num1++);// 해당 문장을 실행 후 변수의 값을 1 증가
+	printf("num1 = %d\n", num1);
+	printf("\n");
+
+	printf("num2 = %d\n", num2);
+	printf("++num2 = %d\n", ++num2);// 변수의 값을 1 증가한 후 문장 실행
+	printf("num2 = %d\n", num2);
+
+	return 0;
+}
+/* 
+ 정보) ++뿐문 아니라 --도 가능하다
+ */
